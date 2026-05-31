b3a9978be51edf22981f020a1c2309dd924bf609
AmanKrs <imkumarak4@gmail.com>
2026-06-01 03:28:46 +0530

    experience  logic update

--- src/Components/ExpCounter/ExpCount.js
+++ src/Components/ExpCounter/ExpCount.js
@@ -10,39 +10,50 @@ export default function ExpCount() {
   useEffect(() => {
     const startDate = new Date("2019-09-02");
     const currentDate = new Date();
-    const diffInMilliseconds = currentDate - startDate;
-    const diffInYears = Math.floor(
-      diffInMilliseconds / (365 * 24 * 60 * 60 * 1000)
-    );
-    const startDateCopy = new Date(startDate.getTime());
-    startDateCopy.setFullYear(startDateCopy.getFullYear() + diffInYears);
-    const diffInMonths = currentDate.getMonth() - startDateCopy.getMonth();
-    const diffInDays = currentDate.getDate() - startDateCopy.getDate();
+
+    let years = currentDate.getFullYear() - startDate.getFullYear();
+    let months = currentDate.getMonth() - startDate.getMonth();
+    let days = currentDate.getDate() - startDate.getDate();
+
+    if (days < 0) {
+      months -= 1;
+      const previousMonthLastDay = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        0,
+      ).getDate();
+      days += previousMonthLastDay;
+    }
+
+    if (months < 0) {
+      years -= 1;
+      months += 12;
+    }
 
     setExperience({
-      years: diffInYears,
-      months: diffInMonths,
-      days: diffInDays,
+      years,
+      months,
+      days,
     });
   }, []);
   return (
     <>
-    <div className="expcount">
-    <h3 className="expCountTitle">Total Experience</h3>
-      <div className="expBox">
-        <div className="expcnt expyear">
-          <p className="cnt">{experience.years}</p>
-          <p className="cnt-title">Years</p>
+      <div className="expcount">
+        <h3 className="expCountTitle">Total Experience</h3>
+        <div className="expBox">
+          <div className="expcnt expyear">
+            <p className="cnt">{experience.years}</p>
+            <p className="cnt-title">Years</p>
+          </div>
+          <div className="expcnt expmnt">
+            <p className="cnt">{experience.months}</p>
+            <p className="cnt-title">Months</p>
+          </div>
+          <div className="expcnt expday">
+            <p className="cnt">{experience.days}</p>
+            <p className="cnt-title">Days</p>
+          </div>
         </div>
-        <div className="expcnt expmnt">
-          <p className="cnt">{experience.months}</p>
-          <p className="cnt-title">Months</p>
-        </div>
-        <div className="expcnt expday">
-          <p className="cnt">{experience.days}</p>
-          <p className="cnt-title">Days</p>
-        </div>
-      </div>
       </div>
     </>
   );
